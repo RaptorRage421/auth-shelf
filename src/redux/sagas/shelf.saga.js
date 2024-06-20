146b79850ff1f686d1a610bc1889fcc1a34cc19c
Sam Shacter <sshacter@gmail.com>
2024-06-20 12:54:13 -0500

    starting reducer/saga build

---
+++ src/redux/sagas/shelf.saga.js
@@ -0,0 +1,12 @@
+
+import { takeLatest, put } from "redux-saga/effects";
+import axios from "axios";
+
+function* fetchShelfItems() {
+    try{
+
+    } catch(error) {
+        console.error ('Error fetching shelf items', error)
+    }
+
+}
